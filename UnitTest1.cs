11d8ea4eb5027f6bf3d79e456be91c730fa62e11
bernakara <77668426+bernakara@users.noreply.github.com>
2021-04-23 08:47:54 +0300

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using CsharpLibrary;
+
+namespace CsharpDllTestSample
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void WrongSumTest()
+        {
+            calculation calc = new calculation();
+            Assert.IsTrue(calc.Add(1, 2) == 3);
+            Console.WriteLine("Add Sample " + calc.Add(1, 2));
+            
+           
+
+        }
+        [TestMethod]
+        public void WrongSubTest()
+        {
+            calculation calc = new calculation();
+            Assert.IsFalse(calc.sub(1, 2) == 3);
+           
+            
+            Console.WriteLine("Sub Sample " + calc.sub(1, 2));
+
+
+        }
+
+    }
+}
